f416f52a543c29457219504b955806b5141b30e7
jolasman <jolasman@hotmail.com>
2014-05-27 13:15:07 +0100

    vs interface intruçoes

--- CAL_P2/CAL_P2/Interface.cpp
+++ CAL_P2/CAL_P2/Interface.cpp
@@ -28,6 +28,27 @@ void screen_exit(){
 	cout << "\n                                                               Daniel Mendonca";
 	cout << "\n                                                               Francisco Lopes\n\n";
 }
+/*apresenta no ecra o menu de opcoes*/
+void screen_intructions(){
+
+	system("CLS");
+	cout << "-----------------------------Encontra a Palavra---------------------------------\n\n";
+	cout << "\nEste e um jogo para dois jogadores, A e B.";
+	cout << "\n\nInicialmente e colocada na mesa uma palavra de N letras (N<5).";
+
+	cout << "\n\nOs jogadores tentam construir novas palavras a partir da palavra da mesa\n";
+	cout << ",isto e, palavras que contenham a palavra da mesa.";
+
+	cout << "\n\nAssim, os jogadores jogam alternadamente adicionando uma letra a palavra\n";
+	cout << "da mesa (no inicio ou no fim).\n\n";
+
+	cout << "\nO jogo termina quando um dos jogadores nao consegue adicionar qualquer letra\n";
+	cout <<	"a palavra da mesa, e perde o jogo.";
+
+	cout << "\n\nO jogador vencedor deve mostrar a palavra em que estava a pensar.\n";
+	cout << "\n\n\n                        (press any key to continue)\n\n";
+	_getch();
+}
 
 /*chama as funcoes para construir o menu*/
 void game_menu(){
@@ -63,8 +84,7 @@ char choice;
 		case 3:
 			cin.clear();
 			cin.ignore(1000, '\n');
-			
-
+			screen_intructions();
 			loop2 = 0;
 			break;
 		case 0:
--- CAL_P2/CAL_P2/Interface.h
+++ CAL_P2/CAL_P2/Interface.h
@@ -5,8 +5,9 @@
 
 void main_menu();
 
-
 void screen_exit();
+
+void screen_intructions();
 
 void game_menu();
 
